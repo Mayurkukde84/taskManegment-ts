4c77683d142e7e05f4f17b503253f004969a9189
mayur <mayurkukde84@gmail.com>
2024-12-18 15:36:12 +0530

    update

--- todo-react/src/App.tsx
+++ todo-react/src/App.tsx
@@ -2,12 +2,28 @@ import React, { FC, ReactElement } from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { customTheme } from './theme/customTheme';
 import Dashboard from './pages/dashboard/dashboard';
+import { 
+  QueryClient, 
+  QueryClientProvider
+} from '@tanstack/react-query';
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'; 
+
+const queryClient = new QueryClient();
+
 const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline/>
-     <Dashboard/>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Dashboard />
+      </ThemeProvider>
+      {/* ReactQueryDevtools configuration updated */}
+      <ReactQueryDevtools 
+        initialIsOpen={false} 
+
+      /> 
+    </QueryClientProvider>
   );
 };
 
